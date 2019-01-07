ae560fa26665f539b1e9b7518a8d1a9a55ad6010
David <toonarmycaptain@hotmail.com>
2019-01-07 14:48:51 -0600

    Fix poor statement ordering. Save 1 loop by initialising at 1, 2. Evaluating new number_b after adding to total worked for Project Euler solution as the value that broke the loop was odd (and thus was not added to the total), but for the test using the first 10 numbers (ie <90), it is even, and thus was added to total before checked by the while condition.

--- problem_2/problem_2_solution.py
+++ problem_2/problem_2_solution.py
@@ -5,16 +5,17 @@ def sum_even_fibonacci_numbers(maximum: int):
     :return: int
     """
 
-    number_a = 0
-    number_b = 1
+    number_a = 1
+    number_b = 2
     total = 0  # initialise total,
     while number_b < maximum:
-        number_a, number_b = number_b, number_a + number_b
         if not number_b % 2:  # if number_b is even
             total += number_b
+        number_a, number_b = number_b, number_a + number_b
+
         # if number_b > max, loop ends, too great number_b not added to total
     return total
 
 
 if __name__ == '__main__':
-    print(sum_even_fibonacci_numbers(3999999))
+    print(sum_even_fibonacci_numbers(90))
