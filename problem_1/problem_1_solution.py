3342ad57a3bdeb20d2dd7152066f62ceeb8515e5
David <toonarmycaptain@hotmail.com>
2019-01-07 11:44:23 -0600

    .gitignore, problem 1 solution.

---
+++ problem_1/problem_1_solution.py
@@ -0,0 +1,41 @@
+# https://projecteuler.net/problem=1
+
+
+def sum_multiples(multiples: tuple, min_max: tuple):
+    """
+    Takes a range and a list of numbers, returns the sum of the numbers
+    within the range that are multiples of any number in the list of
+    multiples.
+
+    :param multiples: tuple (**int)
+    :param min_max: tuple (int, int)
+    :return: int
+    """
+    minimum = min_max[0]
+    maximum = min_max[1]
+    total = 0
+    for i in range(minimum, maximum+1):
+        for j in multiples:
+            if i%j == 0:
+                total += i
+                break
+
+    return total
+
+
+def efficient_sum_multiples_3_or_5(min_max):
+    """
+    One-liner of sum_multiples using list comprehension.
+    :param min_max:
+    :return:
+    """
+    minimum = min_max[0]
+    maximum = min_max[1]
+    return sum([i for i in range(minimum, maximum+1) if i%3 == 0 or i%5 == 0])
+
+
+if __name__ == '__main__':
+    # Solution to problem 1
+    problem_1_multiples = (3, 5)
+    problem_1_min_and_max = (0, 999)
+    print(sum_multiples(problem_1_multiples, problem_1_min_and_max))
---
+++ problem_1/test_problem_1_solution.py
@@ -0,0 +1,17 @@
+from project_euler.problem_1.problem_1_solution import sum_multiples, efficient_sum_multiples_3_or_5
+
+
+def test_sum_multiples():
+    multiples = (3, 5)
+    minimum = 0
+    maximum = 9
+    min_max= (minimum, maximum)
+    assert sum_multiples(multiples, min_max) == 23
+
+
+def test_efficient_sum_multiples_3_or_5():
+    min_max = (0, 9)
+    assert efficient_sum_multiples_3_or_5(min_max) == 23
+
+# test_sum_multiples()
+# test_efficient_sum_multiples_3_or_5
