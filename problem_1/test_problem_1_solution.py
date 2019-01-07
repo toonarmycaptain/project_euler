c2a1bac440a8f45bbea61d9919f89266287f4ed2
David <toonarmycaptain@hotmail.com>
2019-01-07 12:22:27 -0600

    Test both solutions are equal. PEP8 add whitespace.

--- problem_1/test_problem_1_solution.py
+++ problem_1/test_problem_1_solution.py
@@ -5,7 +5,7 @@ def test_sum_multiples():
     multiples = (3, 5)
     minimum = 0
     maximum = 9
-    min_max= (minimum, maximum)
+    min_max = (minimum, maximum)
     assert sum_multiples(multiples, min_max) == 23
 
 
@@ -13,5 +13,11 @@ def test_efficient_sum_multiples_3_or_5():
     min_max = (0, 9)
     assert efficient_sum_multiples_3_or_5(min_max) == 23
 
-# test_sum_multiples()
-# test_efficient_sum_multiples_3_or_5
+
+def test_all_solutions_are_equal():
+    multiples = (3, 5)
+    minimum = 0
+    maximum = 9
+    min_max= (minimum, maximum)
+    assert sum_multiples(multiples, min_max) == efficient_sum_multiples_3_or_5(min_max)
+
