c9a19518c7c05b9daf18bc2076d1b3840377a985
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-01-06 00:18:36 -0600

    Correct incomplete import.

--- problem_8/test_problem_8_solution.py
+++ problem_8/test_problem_8_solution.py
@@ -1,7 +1,7 @@
 """Tests for Project Euler problem 8"""
 import pytest
 
-from problem_8.problem_8_solution import largest_product_in_series, problem_series
+from project_euler.problem_8.problem_8_solution import largest_product_in_series, problem_series
 
 def test_largest_product_in_series():
     assert largest_product_in_series(problem_series, 4) == (5832, '9989')
